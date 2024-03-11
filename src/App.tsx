0fb1d53f0a45c9fad51eb1ff0740eeb3b54f2d71
Pierre Olivier Tran <pierre-olivier@stationf.co>
2024-03-11 17:59:15 +0100

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
 import cover from './assets/hyper.png';
 
+import Countdown from 'react-countdown';
+
 function App() {
+  // releases on April 26 2024
+  const releaseDate = new Date('2024-04-26T00:00:00');
+
   return (
     <main className='dark'>
       <div className='relative flex flex-col h-screen w-screen items-center justify-center bg-white dark:bg-black transition-bg'>
@@ -20,7 +25,13 @@ function App() {
         <h1 className='relative flex items-center text-5xl font-bold text-gray-800 dark:text-white dark:opacity-80 transition-colors'>
           hyperdrama
         </h1>
+        <div className='text-gray-800 dark:text-white dark:opacity-90 transition-colors text-lg'>
+          <Countdown date={releaseDate} />
+        </div>
       </div>
+      <h2 className='fixed left-0 bottom-0 p-4 flex items-center text-2xl font-bold text-gray-800 dark:text-white dark:opacity-80 transition-colors'>
+        potran
+      </h2>
     </main>
   );
 }
